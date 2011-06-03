32b2d764ee4f2742851fe0c0e93bdf33f7cb4dba
Vala <gonjo@free.fr>
2011-06-03 15:18:23 +0200

    Add flash display

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  
+  def display_flash
+    flash_markup = ''
+    flash.each do |name, msg|
+      flash_markup << content_tag(:div, msg, :id => "flash_#{name}", :class => "flash_message") if msg.is_a?(String)
+    end
+    flash_markup.html_safe
+  end
+  
 end
